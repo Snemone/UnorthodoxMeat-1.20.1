b445b4f8a65666537c8434b791cf14462ac36b52
Snemone <g_ostracod@proton.me>
2024-12-19 14:25:27 -0600

    test

---
+++ src/main/java/snemone/tutorialmod/TutorialModClient.java
@@ -0,0 +1,9 @@
+package snemone.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
